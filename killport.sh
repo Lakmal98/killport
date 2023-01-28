fc5ec8a3156bd3cd30e4bdabf78cd43eeab02e3a
Lakmal98 <lakmalepp@gmail.com>
2023-01-28 17:47:18 +0530

    add makefile and upgraded to support multiple port killing at once

--- killport.sh
+++ killport.sh
@@ -1,14 +1,38 @@
-# /bin/bash
+#!/bin/bash
 
-[ $# -eq 0 ] && echo "killport [PORT] : The PORT must given as an argument." && exit 1
-
-[ $# -gt 1 ] && echo "Only one argument [PORT] is valid. $# given in." && exit 1
-
-! [ "$1" -eq "$1" ] 2>>/dev/null && echo "Invalid argument. PORT number should be a number" && exit 1
-
-if [ $(sudo lsof -t -i:$1 | wc -l) -ge 1 ]; then
-  sudo kill -9 $(sudo lsof -t -i:$1)
-  echo "Port $1 has freed up"
-else
-  echo "Port $1 is already free"
+# show an help for argument '-h' or '--help'
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "killport [PORT1 PORT2 ...] : The PORT(s) must given as an argument."
+  exit 0
 fi
+
+# show version
+if [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
+  echo "killport 0.3"
+  exit 0
+fi
+
+if [ $# -eq 0 ]; then
+  echo "killport [PORT1 PORT2 ...] : The PORT(s) must given as an argument."
+  exit 1
+fi
+
+killed_ports=()
+
+for port in "$@"; do
+  ! [ "$port" -eq "$port" ] 2>>/dev/null && echo "Invalid argument. PORT number should be a number" && exit 1
+
+  if [ $(sudo lsof -t -i:$port | wc -l) -ge 1 ]; then
+    sudo kill -9 $(sudo lsof -t -i:$port)
+    echo "Port $port has freed up"
+    killed_ports+=($port)
+  else
+    echo "Port $port is already free"
+  fi
+done
+
+if [ ${#killed_ports[@]} -gt 0 ]; then
+  echo "Killed ports: ${killed_ports[@]}"
+fi
+
+
---
+++ makefile
@@ -0,0 +1,10 @@
+install:
+	mkdir -p killport/usr/local/bin
+	cp killport.sh killport/usr/local/bin/
+	mv killport/usr/local/bin/killport.sh killport/usr/local/bin/killport
+	chmod +x killport/usr/local/bin/killport
+	mkdir -p killport/DEBIAN
+	echo "Package: killport\nVersion: 0.3\nSection: base\nPriority: optional\nArchitecture: all\nDepends: bash\nMaintainer: Dimuthu Lakmal <lakmalepp@gmail.com>\nDescription: A script to kill process associated with a port" > killport/DEBIAN/control
+	echo "#!/bin/bash\nln -s /usr/local/bin/killport.sh /usr/bin/killport" > killport/DEBIAN/postinst
+	chmod +x killport/DEBIAN/postinst
+	dpkg-deb --build killport
